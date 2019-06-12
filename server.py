6fc56b0db0e1bdcf492b649b2e1716b699dc6992
Arthur Svistunov <madbirddiary@gmail.com>
2019-06-13 02:54:24 +0300

    Save unsafe

--- server.py
+++ server.py
@@ -2,9 +2,12 @@ from keras.models import load_model
 from keras.preprocessing import image
 from keras import backend as K
 from PIL import Image
+from pathlib import Path
 import numpy as np
 import flask
 import io
+import os
+import uuid
 
 MODEL_PATH = 'model.hdf5'
 WEIGHTS_PATH = 'weights.hdf5'
@@ -12,6 +15,7 @@ IMAGE_DEPTH = 3
 IMAGE_WIDTH = 192
 IMAGE_HEIGHT = 192
 IMAGE_SHAPE = (IMAGE_DEPTH, IMAGE_HEIGHT, IMAGE_WIDTH)
+NUDES_DIR = 'nudes'
 
 if K.image_data_format() == 'channels_last':
     IMAGE_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH)
@@ -24,14 +28,21 @@ labels = ['nsfw-nude', 'nsfw-risque', 'nsfw-sex', 'nsfw-violence', 'sfw']
 #https://github.com/keras-team/keras/issues/2397#issuecomment-354061212
 print('testing model:', model.predict(np.zeros((1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEPTH))))
 
+def save_file(f):
+    path = os.path.abspath(os.path.join(NUDES_DIR, '{}.png'.format(str(uuid.uuid4()))))
+    with open(path, 'wb') as f_b:
+        f.save(f_b, format='png')
+
 def prepare_image(img):
     if img.mode != "RGB":
         img = img.convert("RGB")
     img = img.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
     img_tensor = image.img_to_array(img)
     img_tensor = np.expand_dims(img_tensor, axis=0)
-    img_tensor /= 255. 
+    img_tensor /= 255.
     return img_tensor
+
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -46,6 +57,8 @@ def predict():
                 data["predictions"][labels[index]] = float(prob)
             data["success"] = True
             data["is_safe"] = bool(pred_prob[0][4] > 0.5)
+            if not data["is_safe"]:
+                save_file(img)
     return flask.jsonify(data)
 
 if __name__ == "__main__":
